89c510162f2b6c45aa64e0d45a02dbeaf8954e68
Aragaw.A <aragaw007@gmail.com>
2025-10-04 20:17:25 +0300

    Fix GET route to return invoices

--- resources/page.tsx
+++ resources/page.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
   return (
     <div>
-      <a href='https://nextjs.org/learn/dashboard-app/optimizing-fonts-images' title='link'></a>
-       <a href='https://nextjs.org/learn/dashboard-app/setting-up-your-database' title='link'></a>
-       <a href='https://stuexam.astu.edu.et' title='link'></a>
+      <a
+        href="https://nextjs.org/learn/dashboard-app/optimizing-fonts-images"
+        title="link"
+      ></a>
+      <a
+        href="https://nextjs.org/learn/dashboard-app/setting-up-your-database"
+        title="link"
+      ></a>
+      <a
+        href="https://vercel.com/aragaw-adem-mohammeds-projects/nextjs-dashboard"
+        title="link"
+      ></a>
     </div>
-  )
+  );
 }
